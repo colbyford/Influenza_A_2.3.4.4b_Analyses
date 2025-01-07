52db8947a65b956c8dbce73c0ff31b54340e0754
Colby Ford <colbytylerford@gmail.com>
2025-01-07 15:28:34 -0500

    Create PQ809550_strainhub.R

---
+++ scripts/PQ809550_strainhub.R
@@ -0,0 +1,29 @@
+## Load in StrainHub
+library(strainhub)
+
+## Read in tree, metadata, and geodata
+treedata <- ape::read.nexus("../data/clade-of-interest-around-PQ809550-strainhub/tree0.nexus")
+metadata <- readr::read_csv("../data/clade-of-interest-around-PQ809550-strainhub/H5N1_N2_HA_nuc_djv28_Pulit-Penaloza-blast-LA-NM_noDup.host.rollup1.clade-of-interest.csv", col_names = TRUE)
+# geodata <- readr::read_csv("", col_names = TRUE)
+
+
+## Check to See Which States are available by which to generate the network
+list_states(treedata,
+            metadata,
+            treeType = "parsimonious")
+
+
+## Make the Transmission Network
+graph <- make_transnet(treedata,
+                       metadata,
+                       columnSelection = "Source_Clean",
+                       centralityMetric = 6,
+                       treeType = "parsimonious")
+
+print(graph)
+
+
+## Make Leaflet/Swoopy map
+# make_map(graph,
+#          geodata = geodata,
+#          columnSelection = "Country")
