494335900b1c3029ad87d6d6732f748c34bb5f2f
Colby Ford <colbytylerford@gmail.com>
2025-01-08 12:24:32 -0500

    Update PQ809550_strainhub.R

--- scripts/PQ809550_strainhub.R
+++ scripts/PQ809550_strainhub.R
@@ -2,8 +2,11 @@
 library(strainhub)
 
 ## Read in tree, metadata, and geodata
-treedata <- ape::read.nexus("../data/clade-of-interest-around-PQ809550-strainhub/tree0.nexus")
-metadata <- readr::read_csv("../data/clade-of-interest-around-PQ809550-strainhub/H5N1_N2_HA_nuc_djv28_Pulit-Penaloza-blast-LA-NM_noDup.host.rollup1.clade-of-interest.csv", col_names = TRUE)
+# treedata <- ape::read.nexus("../data/clade-of-interest-around-PQ809550-strainhub/tree0.nexus")
+# metadata <- readr::read_csv("../data/clade-of-interest-around-PQ809550-strainhub/H5N1_N2_HA_nuc_djv28_Pulit-Penaloza-blast-LA-NM_noDup.host.rollup1.clade-of-interest.csv", col_names = TRUE)
+treedata <- ape::read.tree("../data/clade-of-interest-around-PQ809550-strainhub/Jan8/RAxML_bestTree.H5N1_N2_HA_nuc_djv28_Pulit-Penaloza-clade-of-interest-around-PQ809550.out")
+metadata <- readr::read_csv("../data/clade-of-interest-around-PQ809550-strainhub/Jan8/H5N1_N2_HA_nuc_djv28_Pulit-Penaloza-blast-LA-NM_noDup.host.rollup1.clade-of-interest-good-jan8.csv", col_names = TRUE)
+
 # geodata <- readr::read_csv("", col_names = TRUE)
 
 
@@ -16,7 +19,7 @@ list_states(treedata,
 ## Make the Transmission Network
 graph <- make_transnet(treedata,
                        metadata,
-                       columnSelection = "Source_Clean",
+                       columnSelection = "Source_Clean_FIX",
                        centralityMetric = 6,
                        treeType = "parsimonious")
 
